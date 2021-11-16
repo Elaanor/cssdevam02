5ee466d8facbe56ced366b2e038878bafa5938d5
Elaanor <lol_34_34@hotmail.com>
2021-11-16 17:53:04 +0300

    cssdevam02

---
+++ JS/script.js
@@ -0,0 +1,74 @@
+//console.log("External");
+
+//let deger = "Hello ";
+//console.log(deger);
+//let deger = "Merhaba";
+//console.log(deger);
+
+//var deger = "alloooo";
+//console.log(deger);
+
+//var deger="hmmm";
+//console.log(deger);
+
+// function fonn(){
+// var deger="text";
+
+// }
+// fonn()
+// console.log(d);
+// const text = "Javascript";
+// console.log(text);
+
+
+var ders = "Ders";
+var ders = 'Ders';
+
+var sayi = 1;
+var ondalık = 1.15;
+
+var bool = true;
+
+var a;
+console.log("deger",typeof(a));
+
+var b = null;
+console.log("type",typeof(b));
+
+let id = Symbol();
+
+var lesson ={
+    name:'Web',
+    number:1
+}
+console.log("Object",lesson);
+console.log("Object",lesson.name);
+
+
+var dizi = [1, 2, 3];
+
+console.log("dizi",typeof(dizi));
+
+console.log("item",dizi[1]);
+dizi[3]=4;
+console.log(dizi);
+
+console.log(dizi.length);
+
+
+// function hello(text){
+//     console.log("Function",text);
+// }
+
+// var text = "Text";
+// hello(text);
+
+var num = true;
+var ynum = Number(num);
+console.log(ynum);
+
+var str = 1;
+var ss = String(str);
+console.log(ss);
+
+console.log(ss);
